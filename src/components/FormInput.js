120a1de0b299059b33f11c94ebdafe3c3a3fd53c
kodowicz <annakodowicz@gmail.com>
2018-09-16 21:40:37 +0200

    basic listing search results

--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import { FormContext } from '../App';
 
 class FormInput extends Component {
 
   render() {
     return (
-      <input
-        type="text" placeholder="" aria-label="search city or state"
-      />
+      <FormContext.Consumer>
+        {(context) => (
+          <input
+            onChange={context.inputHasContent}
+            type="text" placeholder="" aria-label="search city or state"
+          />
+        )}
+      </FormContext.Consumer>
     );
   }
 }
--- src/components/FormList.js
+++ src/components/FormList.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
+import { FormContext } from '../App';
 
 
 class FormList extends Component {
   state = {
   }
 
+  findMatches = (wordToMatch, cities) => {
+    return cities.filter(place => {
+      const matchPattern = new RegExp(wordToMatch, 'gi');
+      return place.city.match(matchPattern) || place.state.match(matchPattern)
+    });
+  };
+  
   render() {
     return (
-      <ul>
-        <FormListItem/>
-      </ul>
+      <FormContext.Consumer>
+        {(context) => {
+          const matchedPlaces = this.findMatches(context.state.input, context.state.cities);
+          return (
+            <React.Fragment>
+              <ul>
+                {matchedPlaces.map(place =>
+                  <FormListItem
+                    key={place.rank}
+                    place={place}
+                  />
+                )}
+              </ul>
+            </React.Fragment>
+          );
+        }}
+      </FormContext.Consumer>
     );
   }
 }
@@ -21,7 +43,7 @@ class FormListItem extends Component {
 
   render() {
     return (
-      <li>city, state</li>
+      <li>{this.props.place.city}, {this.props.place.state}</li>
     );
   }
 }

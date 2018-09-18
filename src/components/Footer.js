2ed039802c056456a7fc658259a327becc4abc57
kodowicz <annakodowicz@gmail.com>
2018-09-18 23:34:10 +0200

    header and footer styles added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
+
+
+const PageFooter = styled.footer`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 60px;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  
+  & * {
+    color: #ECEFF1;
+    font-size: 14px;
+    font-weight: 400;
+    text-decoration: none
+  }
+
+  & a:hover {
+    color: #B0BEC5
+  }
+`;
 
 const Footer = () => (
-  <footer>
+  <PageFooter>
     <a href="https://kodowicz.github.io/plantgang/">← prev project</a>
     <span>Anna Kotowicz ©  2018</span>
     <a href="">next project →</a>
-  </footer>
+  </PageFooter>
 );
 
 export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  margin: 0;
+  padding-top: 130px;
+  padding-bottom: 30px
+  text-align: center;
+  color: #04060B;
+  font-size: 26px;
+  font-weight: 300;
+  font-family: 'Open Sans', sans-serif;
+`;
 
 const Header = () => (
   <header>
-    <h1>Find basic information about cities in all states in USA.</h1>
+    <Title>Find basic information about cities in all states in USA.</Title>
   </header>
 );
 

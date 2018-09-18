bb2e2163f0f2a696b9c282a62746680f54a8b320
kodowicz <annakodowicz@gmail.com>
2018-09-18 23:35:28 +0200

    background image

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import { FormProvider } from '../App';
 import FormInput from './FormInput';
 import FormList from './FormList';
 
-const Main = () => (
-  <main>
-    <form role="search">
-      <FormInput />
-      <FormList />
-    </form>
-  </main>
-)
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 450px;
+  margin: 0 auto;
+`;
+
+class Main extends Component {
+  render() {
+    return (
+      <main>
+        <FormProvider>
+          <Form role="search">
+            <FormInput />
+            <FormList />
+          </Form>
+        </FormProvider>
+      </main>
+    );
+  }
+};
 
 export default Main;
